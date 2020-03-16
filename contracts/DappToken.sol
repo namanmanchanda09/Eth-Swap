2d4c417de2076df6804b8eaad59fccee2958f08b
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-16 22:49:06 +0530

    added comments

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,15 +1,16 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract DappToken{
+    uint256 public totalSupply; //Total Supply of Tokens
+    mapping(address=>uint256) public balanceOf; //Mapping of Tokens and Token holders addresses
 
-    uint256 public totalSupply;
-    mapping(address=>uint256) public balanceOf;
-
+    //Constructor to initialise the Initial Supply of Tokens
     constructor(uint256 _initialSupply) public{
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 }
+
 
 
 
--- test/DappToken.js
+++ test/DappToken.js
@@ -11,10 +11,12 @@ contract('DappToken',(accounts)=>{
     })
 
     describe('deployment',async()=>{
+        //Checking the total Supply of Tokens
         it('sets the total supply after deployment',async()=>{
             const totalSupply = await dappToken.totalSupply()
             assert.equal(totalSupply.toNumber(),1000000);
         })
+        //Checking the initial Admin Balance
         it('admin balance is equal to 1000000',async()=>{
             const adminBalance = await dappToken.balanceOf(accounts[0]);
             assert.equal(adminBalance.toNumber(),1000000)
