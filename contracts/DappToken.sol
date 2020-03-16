82baa3a9c66541ebbce447e255dc28f0a583b04a
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-17 00:03:51 +0530

    added comments in Contract

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,9 +2,9 @@ pragma solidity >=0.4.21 <0.7.0;
 
 
 contract DappToken{
-    string public name = 'DAppToken';
-    string public symbol = 'DAPP';
-    string public standard = 'DApp Token v1.0';
+    string public name = 'DAppToken'; //Token Name
+    string public symbol = 'DAPP'; //Token Symbol
+    string public standard = 'DApp Token v1.0'; //Token Symbol ~ Not a ERC20 Standard
     uint256 public totalSupply; //Total Supply of Tokens
     mapping(address=>uint256) public balanceOf; //Mapping of Tokens and Token holders addresses
 
