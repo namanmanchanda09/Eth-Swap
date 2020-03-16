c66a27a87c538f0eea80b6ae013507cc0b1beae7
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-16 23:49:48 +0530

    added symbol and tests

--- test/DappToken.js
+++ test/DappToken.js
@@ -11,23 +11,34 @@ contract('DappToken',(accounts)=>{
     })
 
     describe('deployment',async()=>{
+
         //Checking the name of the Token
         it('initialises the contract with correct values',async()=>{
             const name = await dappToken.name();
             assert.equal(name,'DAppToken')
         })
+
+        //Checking the symbol of the Token
+        it('initialises the contract with correct values',async()=>{
+            const symbol = await dappToken.symbol();
+            assert.equal(symbol,'DAPP')
+        })
+
         //Checking the total Supply of Tokens
         it('allocates the total supply after deployment',async()=>{
             const totalSupply = await dappToken.totalSupply()
             assert.equal(totalSupply.toNumber(),1000000);
         })
+
         //Checking the initial Admin Balance
         it('admin balance is equal to 1000000',async()=>{
             const adminBalance = await dappToken.balanceOf(accounts[0]);
             assert.equal(adminBalance.toNumber(),1000000)
         })
-        
     })
 })
+
+
+
 
 
