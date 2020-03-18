3ba6f8d750b2f1b881266b751970ffb411ebf007
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-18 22:51:18 +0530

    written buyingTokens tests

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -21,3 +21,4 @@ contract EthSwap {
 
 
 
+
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -9,7 +9,7 @@ function tokens(n){
     return web3.utils.toWei(n,'ether');
 }
 
-contract('EthSwap',(accounts)=>{
+contract('EthSwap',([deployer,investor])=>{
     let token, ethSwap;
     before(async()=>{
         token = await Token.new();
@@ -35,7 +35,34 @@ contract('EthSwap',(accounts)=>{
             assert.equal(balance.toString(),tokens('1000000'));
         })
     })
+    //Testing buying Tokens
+    describe('buyTokens()',async()=>{
+        let result;
+        before(async()=>{
+            //Purchase tokens before each example
+            result = await ethSwap.buyTokens({from:investor,value:web3.utils.toWei('1','ether')});
+        })
+
+        it('Allows users to instantly purchase tokens from ethSwap',async()=>{
+            //Check investor token balance after purchase
+            let investorBalance = await token.balanceOf(investor);
+            assert.equal(investorBalance.toString(),tokens('100'));
+            //Check ethSwap Balance after purchase
+            let ethSwapBalance = await token.balanceOf(ethSwap.address);
+            assert.equal(ethSwapBalance.toString(),tokens('999900'));
+            //Check ethSwap ether balance after transaction
+            ethSwapBalance = await web3.eth.getBalance(ethSwap.address);
+            assert.equal(ethSwapBalance.toString(),web3.utils.toWei('1','ether'));
+        })
+    })
 })
+
+
+
+
+
+
+
 
 
 
