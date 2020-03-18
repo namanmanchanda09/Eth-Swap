5df724dec257760e369843451483fadf5fd38662
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-18 22:17:38 +0530

    written contract for buying tokens

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -4,11 +4,19 @@ import './Token.sol';
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
     Token public token;
+    uint public rate = 100;
 
     constructor(Token _token) public {
         token = _token;
     }
+
+    function buyTokens() public payable{
+        uint tokenAmount = msg.value * rate;
+        token.transfer(msg.sender,tokenAmount);
+    }
 }
+
+
 
 
 
