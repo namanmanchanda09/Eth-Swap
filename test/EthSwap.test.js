b23d6d8ae6867e4c9d363de299a4ee886198a469
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-18 19:57:26 +0530

    added a helper function

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -3,14 +3,18 @@ const EthSwap = artifacts.require('EthSwap');
 
 require('chai')
     .use(require('chai-as-promised'))
-    .should()
+    .should();
+
+function tokens(n){
+    return web3.utils.toWei(n,'ether');
+}
 
 contract('EthSwap',(accounts)=>{
     let token, ethSwap;
     before(async()=>{
         token = await Token.new();
         ethSwap = await EthSwap.new();
-        await token.transfer(ethSwap.address,'1000000000000000000000000');
+        await token.transfer(ethSwap.address,tokens('1000000'));
     })
     //Testing Token Deployment
     describe('Token deployment',async()=>{
@@ -28,9 +32,12 @@ contract('EthSwap',(accounts)=>{
         })
         it('contract has tokens',async()=>{
             let balance = await token.balanceOf(ethSwap.address);
-            assert.equal(balance.toString(),'1000000000000000000000000');
+            assert.equal(balance.toString(),tokens('1000000'));
         })
     })
 })
+
+
+
 
 
