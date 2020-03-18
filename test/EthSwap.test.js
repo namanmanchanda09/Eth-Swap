3024375ad0c9a4d9fc716cd29b634779e7e40b61
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-18 13:47:49 +0530

    refactored

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -7,31 +7,28 @@ require('chai')
 
 contract('EthSwap',(accounts)=>{
 
+    before(async()=>{
+        let token = await Token.new();
+        let ethSwap = await EthSwap.new();
+        await token.transfer(ethSwap.address,'1000000000000000000000000');
+    })
+    //Testing Token Deployment
     describe('Token deployment',async()=>{
         it('contract has a name',async()=>{
-            let token = await Token.new()
             const name = await token.name();
             assert.equal(name,'DApp Token');
         })
 
     })
-    
+    //Checking EthSwap Deployment
     describe('EthSwap deployment',async()=>{
         it('contract has a name',async()=>{
-            let ethSwap = await EthSwap.new()
             const name = await ethSwap.name();
             assert.equal(name,'EthSwap Instant Exchange');
         })
-
         it('contract has tokens',async()=>{
-            let token = await Token.new();
-            let ethSwap = await EthSwap.new()
-            await token.transfer(ethSwap.address,'1000000000000000000000000');
             let balance = await token.balanceOf(ethSwap.address);
             assert.equal(balance.toString(),'1000000000000000000000000');
-
         })
-
     })
-
 })
