1a47a1f03d1530838a9157cdad2f83b5572236b2
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-20 13:37:42 +0530

    connected app to blockchain using Web3

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
+
 import './App.css';
 
 class App extends Component {
+
+  async componentWillMount(){
+    await this.loadWeb3();
+    console.log(window.web3);
+  }
+
+  async loadWeb3() {
+    //Connecting app with the blockchain
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
   render() {
     return (
       <div>
@@ -36,3 +57,4 @@ class App extends Component {
 }
 
 export default App;
+
